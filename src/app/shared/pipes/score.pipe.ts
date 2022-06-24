bdb9a12f15fda8ee9db74f77f7c0c03bb5fe0fc4
Axurynn <marion.bricout@outlook.com>
2022-06-24 10:09:45 +0200

    Pipe score bon formalisme

--- src/app/shared/pipes/score.pipe.ts
+++ src/app/shared/pipes/score.pipe.ts
@@ -5,10 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ScorePipe implements PipeTransform {
   transform(score: number, ...args: unknown[]): string {
-    if (score <= 0) {
-      return `${score}`;
+    if (score < 0) {
+      return `- ${Math.abs(score)}`;
+    } else if (score > 0) {
+      return `+ ${score}`;
     } else {
-      return `+${score}`;
+      return `${score}`;
     }
   }
 }
